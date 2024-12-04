ae262a9f35cbfcfec8baf6c1b8ea7c331646f684
Михайлов Артём <artem.mikh100@gmail.com>
2024-12-04 12:18:39 +0300

    Refactor argument parser setup

--- configs.py
+++ configs.py
@@ -1,70 +1,73 @@
 import argparse
 
 
-def configure_argument_parser():
-    def add_edit_arguments(parser):
-        parser.add_argument(
-            '--id', type=int, required=True,
-            help='ID задачи для редактирования'
-        )
-        parser.add_argument('-t', '--title', help='Новое название задачи')
-        parser.add_argument('-d', '--description',
-                            help='Новое описание задачи')
-        parser.add_argument('-c', '--category', help='Новая категория задачи')
-        parser.add_argument(
-            '-dt', '--due_date', help='Новый срок выполнения задачи'
-        )
-        parser.add_argument(
-            '-p', '--priority', help='Новый приоритет задачи',
-            choices=['низкий', 'средний', 'высокий']
-        )
-        parser.add_argument(
-            '-s', '--status', help='Новый статус задачи',
-            choices=['выполнена', 'не выполнена']
-        )
+def add_edit_arguments(parser: argparse.ArgumentParser) -> None:
+    parser.add_argument(
+        '--id', type=int, required=True,
+        help='ID задачи для редактирования'
+    )
+    parser.add_argument('-t', '--title', help='Новое название задачи')
+    parser.add_argument('-d', '--description', help='Новое описание задачи')
+    parser.add_argument('-c', '--category', help='Новая категория задачи')
+    parser.add_argument(
+        '-dt', '--due_date', help='Новый срок выполнения задачи'
+    )
+    parser.add_argument(
+        '-p', '--priority', help='Новый приоритет задачи',
+        choices=['низкий', 'средний', 'высокий']
+    )
+    parser.add_argument(
+        '-s', '--status', help='Новый статус задачи',
+        choices=['выполнена', 'не выполнена']
+    )
 
-    def add_list_arguments(parser):
-        group = parser.add_mutually_exclusive_group(required=True)
-        group.add_argument(
-            '-a',
-            '--all',
-            action='store_true',
-            help='список всех задач')
-        group.add_argument(
-            '-c',
-            '--category',
-            help='список задач по категории')
 
-    def add_task_arguments(parser):
-        parser.add_argument('-t', '--title', required=True, help='название')
-        parser.add_argument('-d', '--description', required=True,
-                            help='описание')
-        parser.add_argument('-c', '--category', required=True,
-                            help='категория')
-        parser.add_argument('-dt', '--due_date', required=True,
-                            help='срок выполнения')
-        parser.add_argument(
-            '-p', '--priority', required=True, help='приоритет',
-            choices=['низкий', 'средний', 'высокий']
-        )
-        parser.add_argument(
-            '-s', '--status', required=True, help='статус',
-            choices=['выполнена', 'не выполнена']
-        )
+def add_list_arguments(parser: argparse.ArgumentParser) -> None:
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+        '-a',
+        '--all',
+        action='store_true',
+        help='Список всех задач'
+    )
+    group.add_argument(
+        '-c',
+        '--category',
+        help='Список задач по категории'
+    )
 
-    def add_search_arguments(parser):
-        group = parser.add_mutually_exclusive_group(required=True)
-        group.add_argument('-kw', '--keyword',
-                           help='ключевое слово для поиска задачи')
-        group.add_argument('-c', '--category',
-                           help='категория задачи для поиска')
-        group.add_argument('-s', '--status', help='статус задачи для поиска')
 
-    def add_remove_arguments(parser):
-        group = parser.add_mutually_exclusive_group(required=True)
-        group.add_argument('--id', type=int, help='ID задачи для удаления')
-        group.add_argument('--category', help='Категория задач для удаления')
+def add_task_arguments(parser: argparse.ArgumentParser) -> None:
+    parser.add_argument('-t', '--title', required=True, help='Название')
+    parser.add_argument('-d', '--description', required=True, help='Описание')
+    parser.add_argument('-c', '--category', required=True, help='Категория')
+    parser.add_argument('-dt', '--due_date', required=True,
+                        help='Срок выполнения')
+    parser.add_argument(
+        '-p', '--priority', required=True, help='Приоритет',
+        choices=['низкий', 'средний', 'высокий']
+    )
+    parser.add_argument(
+        '-s', '--status', required=True, help='Статус',
+        choices=['выполнена', 'не выполнена']
+    )
 
+
+def add_search_arguments(parser: argparse.ArgumentParser) -> None:
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-kw', '--keyword',
+                       help='Ключевое слово для поиска задачи')
+    group.add_argument('-c', '--category', help='Категория задачи для поиска')
+    group.add_argument('-s', '--status', help='Статус задачи для поиска')
+
+
+def add_remove_arguments(parser: argparse.ArgumentParser) -> None:
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--id', type=int, help='ID задачи для удаления')
+    group.add_argument('--category', help='Категория задач для удаления')
+
+
+def configure_argument_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description='Менеджер задач')
     subparsers = parser.add_subparsers(dest='command', required=True)
     list_parser = subparsers.add_parser('list', help='Посмотреть список задач')
@@ -77,5 +80,4 @@ def configure_argument_parser():
     add_remove_arguments(remove_parser)
     edit_parser = subparsers.add_parser('edit', help='Изменение задачи')
     add_edit_arguments(edit_parser)
-
     return parser
