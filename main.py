09ccca2c7f94c9e462adb8f2abd5adc3144ffcf5
Михайлов Артём <artem.mikh100@gmail.com>
2024-12-11 18:23:19 +0300

    Add command_dispatcher

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from argparse import Namespace
 
-from configs import configure_argument_parser
+from ui.configs import configure_argument_parser
 from task_manager import TaskManager
+from ui.command_dispatcher import CommandDispatcher
 
 
 def main() -> None:
@@ -9,33 +10,8 @@ def main() -> None:
     arg_parser = configure_argument_parser()
     args: Namespace = arg_parser.parse_args()
     manager: TaskManager = TaskManager()
-
-    if args.command == 'add':
-        manager.add_task(
-            title=args.title,
-            description=args.description,
-            category=args.category,
-            due_date=args.due_date,
-            priority=args.priority,
-            status=args.status,
-        )
-    elif args.command == 'list':
-        manager.list_task(all=args.all, category=args.category)
-    elif args.command == 'remove':
-        manager.remove_task(id=args.id, category=args.category)
-    elif args.command == 'search':
-        manager.search_task(keyword=args.keyword, category=args.category,
-                            status=args.status)
-    elif args.command == 'edit':
-        manager.edit_task(
-            id=args.id,
-            title=args.title,
-            description=args.description,
-            category=args.category,
-            due_date=args.due_date,
-            priority=args.priority,
-            status=args.status,
-        )
+    dispatcher = CommandDispatcher(manager)
+    dispatcher.dispatch(args)
 
 
 if __name__ == '__main__':
---
+++ ui/command_dispatcher.py
@@ -0,0 +1,26 @@
+from argparse import Namespace
+
+from task_manager import TaskManager
+
+
+class CommandDispatcher:
+    """Обрабатывает команды и вызывает соответствующие методы TaskManager."""
+
+    def __init__(self, manager: TaskManager) -> None:
+        self.manager = manager
+        self.commands = {
+            'add': self.manager.add_task,
+            'remove': self.manager.remove_task,
+            'list': self.manager.list_task,
+            'search': self.manager.search_task,
+            'edit': self.manager.edit_task
+        }
+
+    def dispatch(self, args: Namespace) -> None:
+        """Вызывает метод TaskManager, соответствующий команде."""
+        command = args.command
+        manager_method = self.commands.get(command)
+        if manager_method:
+            manager_method(**vars(args))
+        else:
+            print(f"Команда {command} не поддерживается")
