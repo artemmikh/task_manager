d1d9035e02c6d9ef226c8c0581c3c3386f33e410
Михайлов Артём <artem.mikh100@gmail.com>
2024-12-12 13:39:52 +0300

    Add dataclass in Task

--- models/task.py
+++ models/task.py
@@ -1,34 +1,17 @@
-from typing import Dict, Union
+from dataclasses import dataclass, asdict
 
 
+@dataclass
 class Task:
     """Представляет задачу с различными аттрибутами."""
+    id: int
+    title: str
+    description: str = "Нет описания"
+    category: str = "Общая"
+    due_date: str = "Не указана"
+    priority: str = "Низкий"
+    status: str = "Новая"
 
-    def __init__(self,
-                 id: int,
-                 title: str,
-                 description: str,
-                 category: str,
-                 due_date: str,
-                 priority: str,
-                 status: str) -> None:
-        """Инициализирует задачу с указанными аттрибутами."""
-        self.id: int = id
-        self.title: str = title
-        self.description: str = description
-        self.category: str = category
-        self.due_date: str = due_date
-        self.priority: str = priority
-        self.status: str = status
-
-    def to_dict(self) -> Dict[str, Union[int, str]]:
-        """Конвертирует задачу в словарь."""
-        return {
-            "id": self.id,
-            "title": self.title,
-            "description": self.description,
-            "category": self.category,
-            "due_date": self.due_date,
-            "priority": self.priority,
-            "status": self.status
-        }
+    def to_dict(self) -> dict:
+        """Преобразует объект Task в словарь."""
+        return asdict(self)
